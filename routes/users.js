6fed51b9685d09d6208ba75c1baea7145bfff264
Bhavan Pandya <pandyabhavan134@gmail.com>
2017-12-06 10:16:22 -0800

    first commitdklfa

--- routes/users.js
+++ routes/users.js
@@ -8,23 +8,11 @@ router.get('/', function(req, res, next) {
 
 
 router.post('/login',function (req,res) {
-    var db = req.db;
-    const users = db.get('users');
-    users.find({email:req.body.email,password:req.body.password},{},function (err,docs) {
-        if(docs.length != 0)
-        {
-            req.session.user = req.body.email;
-            users.update({_id:docs[0]._id},{$set:{last_login:new Date()}},function (err,docs) {
-                users.find({}, {},function (e,docs) {
-                    res.sendStatus(200);
-                });
-            });
-        }
-        else
-        {
-            res.render('login',{msg:"Invalid Login",visible:"inline"});
-        }
-    });
+    if(req.body.email == "bhavan" && req.body.password == "b")
+        res.status(200);
+    else
+        res.status(401);
+        res.send();
 });
 
 
