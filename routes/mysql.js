005617fb18bd8d2391148cbcd2ef281d92055513
Bhavan Pandya <pandyabhavan134@gmail.com>
2017-12-06 14:10:31 -0800

    MySQL

---
+++ routes/mysql.js
@@ -0,0 +1,32 @@
+var mysql = require('mysql');
+
+function getConnection(){
+	var connection = mysql.createConnection({
+		host     : 'library.c6ayadrbxmzl.us-west-1.rds.amazonaws.com',
+		user     : 'master',
+		password : 'master123',
+		database : 'library',
+		port  : 3306 
+		});
+	return connection;
+	
+}
+
+ function fetchData(callback,sqlQuery){
+	 console.log("\nSQL Query::"+sqlQuery);
+	 var connection=getConnection();
+	 console.log('Connection established');
+	 connection.query(sqlQuery, function(err, rows, fields) {
+		 if(err){ 
+			 console.log("ERROR: " + err.message);
+			 callback(err, err.message);
+			 }
+		 else   {
+			 console.log("DB Results:"+rows);
+			 callback(err, rows);
+			 }
+		 }); 
+	 	console.log("\nConnection closed.."); 
+	 	connection.end();
+ 	} 
+ exports.fetchData=fetchData;
--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,55 @@
 var express = require('express');
 var router = express.Router();
-var users = [];
-var fs = require('fs');
-users = require('../users.json');
+var mysql = require('./mysql');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
 });
 
 
 router.post('/login',function (req,res) {
-    console.log(JSON.stringify(users));
-    for(var i=0;i<users.length;i++) {
-        console.log(JSON.stringify(users[i]));
-
-        if(req.body.email === users[i].email && req.body.password === users[i].password){
-            res.send({"status":"200","data":users[i]})
+    var query = "select * from user where email='"+req.body.email+"')";
+    mysql.fetchData(function(err,results){
+        if(err)
+        {
+            console.log('in error');
+            res.send({"status":"401","data":null});
         }
         else
-            res.send({"status":"401","data":null})
-    }
+        {
+            if(results.length > 0)
+            {
+                if(password == results[0].password)
+                {
+                    res.send({"status":"200","data":results[0]})
+                }
+                else
+                {
+                    res.send({"status":"401","data":null})
+                }
+            }
+            else
+            {
+                res.send({"status":"401","data":null})
+            }
+        }
+    },query);
 });
 
 router.post('/register',function (req,res) {
-    console.log(req.body);
-    users.push(req.body);
-    fs.writeFile('users.json', JSON.stringify(users));
-    res.send({"status":"200","data":req.body});
+    var query = "insert into user values('"+req.body.email+"','"+req.body.password+"','"+req.body.university+"');";
+    mysql.fetchData(function(err,results){
+        if(err)
+        {
+            res.send({"status":"401","data":null});
+        }
+        else
+        {
+            console.log(results);
+            res.send({"status":"200","data":req.body});
+        }
+    },query);
 });
 
 module.exports = router;
