1dd29ed1dc217844ce425fa759e784be7ef5bd19
Bhavan Pandya <pandyabhavan134@gmail.com>
2017-12-06 22:59:00 -0800

    books update

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/books',require('./routes/books'));
 
 app.use(function (req,res,next) {
     req.db = db;
