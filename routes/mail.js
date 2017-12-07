822a54698c2e53545572631018b5b0b738f73274
Bhavan Pandya <pandyabhavan134@gmail.com>
2017-12-06 18:52:20 -0800

    Email & Verification Code

---
+++ routes/mail.js
@@ -0,0 +1,29 @@
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+
+
+function sendEmail(to,subject,text) {
+    var transporter = nodemailer.createTransport(smtpTransport({
+        service: 'gmail',
+        auth: {
+            user: 'pandyabhavan134@gmail.com',
+            pass: 'impoSSible'
+        }
+    }));
+
+    var mailOptions = {
+        from: 'pandyabhavan134@gmail.com',
+        to: to,
+        subject: subject,
+        text: text
+    };
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+}
+
+exports.sendEmail = sendEmail;
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('./mysql');
+var mail = require('./mail');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.send('respond with a resource');
 });
-
 
 router.post('/login',function (req,res) {
     var query = "select * from user where email='"+req.body.email+"' and password='"+req.body.password+"'";
@@ -31,7 +31,7 @@ router.post('/login',function (req,res) {
 });
 
 router.post('/register',function (req,res) {
-    var query = "insert into user values('"+req.body.email+"','"+req.body.password+"','"+req.body.university+"');";
+    var query = "select * from user where university='"+req.body.university+"'";
     mysql.fetchData(function(err,results){
         if(err)
         {
@@ -39,8 +39,27 @@ router.post('/register',function (req,res) {
         }
         else
         {
-            console.log(results);
-            res.send({"status":"200","data":req.body});
+            if(results.length == 0)
+            {
+                if(req.body.email.includes(""))
+                var query1 = "insert into user values('"+req.body.email+"','"+req.body.password+"','"+req.body.university+"');";
+                mysql.fetchData(function(err,results){
+                    if(err)
+                    {
+                        res.send({"status":"401","data":null});
+                    }
+                    else
+                    {
+                        console.log(results);
+                        var verification_code = Math.floor(Math.random()*10000);
+                        mail.sendEmail(req.body.email,'Registration verfication','Your verification code is:'+verification_code);
+                        req.body["verification_code"] = verification_code;
+                        res.send({"status":"200","data":req.body});
+                    }
+                },query1);
+            }
+            else
+                res.send({"status":"403","data":null});
         }
     },query);
 });
